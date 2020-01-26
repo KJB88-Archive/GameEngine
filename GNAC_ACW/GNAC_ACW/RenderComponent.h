c54dfbb66b32154b0610126f46a2f0984228c204
Kevin <KJB88@users.noreply.github.com>
2020-01-26 21:49:18 +0000

    .
    .

---
+++ GNAC_ACW/GNAC_ACW/RenderComponent.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Component.h"
+
+class RenderComponent
+	: Component
+{
+public:
+	RenderComponent();
+	virtual ~RenderComponent();
+
+private:
+
+	// Shader* shader;
+	// Mesh* mesh;
+	// Material* mat // color, tex, etc
+};
